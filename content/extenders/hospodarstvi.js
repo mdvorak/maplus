f981673479185457dd5549e5acace42448afb416
ext91596 <ext91596@BUDWD284>
2007-08-08 11:18:32 +0200

    Hospodarstvi - aktivni jednotky, komplet barvy

--- content/extenders/hospodarstvi.js
+++ content/extenders/hospodarstvi.js
@@ -38,16 +38,73 @@ pageExtenders.add(PageExtender.create({
     getName: function() { return "Hospodarstvi - Analyza"; },
 
     analyze: function(page, context) {
-        var hospodarstvi = {
+        page.hospodarstvi = {
             tableStavby: $X('table[1]', page.content),
             tableJednotky: $X('table[2]', page.content),
             tablePrijem: $X('table[3]', page.content),
         };
         
-        if (!hospodarstvi.tableStavby || !hospodarstvi.tableJednotky || !hospodarstvi.tablePrijem)
+        if (!page.hospodarstvi.tableStavby 
+                || !page.hospodarstvi.tableJednotky 
+                || !page.hospodarstvi.tablePrijem) {
             return false;
+        }
         
-        page.hospodarstvi = hospodarstvi;
+        var jednotkyRows = page.hospodarstvi.tableJednotky.rows;
+        if (jednotkyRows.length < 2)
+            return false;
+            
+        var trJednotkyCelkem = jednotkyRows[jednotkyRows.length - 1];
+        
+        var jednotky = new Array();
+        
+        jednotky.silaCelkem = parseInt(XPath.evalString('td[3]/font', trJednotkyCelkem));
+        jednotky.pocetCelkem = parseInt(XPath.evalString('td[4]/font', trJednotkyCelkem));
+        jednotky.zlataCelkem = parseFloat(XPath.evalString('td[5]/font', trJednotkyCelkem));
+        jednotky.manyCelkem = parseFloat(XPath.evalString('td[6]/font', trJednotkyCelkem));
+        jednotky.popCelkem = parseFloat(XPath.evalString('td[7]/font', trJednotkyCelkem));
+        
+        console.debug("Jednotky za tah: sila=%d pocet=%d zlata=%f many=%f pop=%f", jednotky.silaCelkem, jednotky.pocetCelkem, jednotky.zlataCelkem, jednotky.manyCelkem, jednotky.popCelkem);
+        
+        for (var i = 1; i < jednotkyRows.length; i++) {
+            if (jednotkyRows[i].cells.length < 10)
+                continue;
+            
+            // Najdi bunky
+            jednotkyRows[i].cells.jmeno = jednotkyRows[i].cells[0];
+            jednotkyRows[i].cells.zkusenost = jednotkyRows[i].cells[1];
+            jednotkyRows[i].cells.phb = jednotkyRows[i].cells[2];
+            jednotkyRows[i].cells.druh = jednotkyRows[i].cells[3];
+            jednotkyRows[i].cells.typ = jednotkyRows[i].cells[4];
+            jednotkyRows[i].cells.sila = jednotkyRows[i].cells[5];
+            jednotkyRows[i].cells.pocet = jednotkyRows[i].cells[6];
+            jednotkyRows[i].cells.zlata = jednotkyRows[i].cells[7];
+            jednotkyRows[i].cells.many = jednotkyRows[i].cells[8];
+            jednotkyRows[i].cells.pop = jednotkyRows[i].cells[9];
+            
+            // Vytvor informace o jednotce
+            jednotkyRows[i].data = {
+                jmeno: jednotkyRows[i].cells.jmeno.textContent,
+                zkusenost: parseFloat(jednotkyRows[i].cells.zkusenost.textContent),
+                phb: parseInt(jednotkyRows[i].cells.phb.textContent),
+                druh: jednotkyRows[i].cells.druh.textContent,
+                typ: jednotkyRows[i].cells.typ.textContent,
+                sila: parseInt(jednotkyRows[i].cells.zlata.textContent),
+                pocet: parseInt(jednotkyRows[i].cells.zlata.textContent),
+                zlata: parseFloat(jednotkyRows[i].cells.zlata.textContent),
+                many: parseFloat(jednotkyRows[i].cells.many.textContent),
+                pop: parseFloat(jednotkyRows[i].cells.pop.textContent),
+            };
+            
+            jednotkyRows[i].data.cenaZaSilu = -parseFloat(jednotkyRows[i].data.zlata) / jednotkyRows[i].data.sila * 1000
+            
+            jednotky.push(jednotkyRows[i]);
+        }
+        
+        page.hospodarstvi.jednotky = jednotky;
+        console.debug("Pocet stacku: %d", jednotky.length);
+        
+        return true;
     },
 
     process: null
@@ -82,63 +139,64 @@ pageExtenders.add(PageExtender.create({
         if (!page.hospodarstvi)
             return false;
     
-        var rows = page.hospodarstvi.tableJednotky.rows;
+        context.zaTah = {
+            tdZlata: $X('tbody/tr[5]/td[2]', page.hospodarstvi.tablePrijem),
+            tdMany: $X('tbody/tr[5]/td[3]', page.hospodarstvi.tablePrijem),
+            tdPop: $X('tbody/tr[5]/td[4]', page.hospodarstvi.tablePrijem)
+        };
         
-        context.trCelkem = rows[rows.length - 1];
-        
-        context.pocetJednotek = rows.length - 2;
-        if (context.pocetJednotek == 0)
+        if (!context.zaTah.tdZlata || !context.zaTah.tdMany || !context.zaTah.tdPop)
             return false;
         
-        context.zlataCelkem = parseFloat(XPath.evalString('td[5]/font', context.trCelkem));
-        context.manyCelkem = parseFloat(XPath.evalString('td[6]/font', context.trCelkem));
-        context.popCelkem = parseFloat(XPath.evalString('td[7]/font', context.trCelkem));
-        
-        context.jednotky = new Array();
-        
-        for (var i = 0; i < rows.length; i++) {
-            if (rows[i].cells.length < 10)
-                continue;
-                
-            rows[i].cells.zkusenost = rows[i].cells[1];
-            rows[i].cells.druh = rows[i].cells[3];
-            rows[i].cells.typ = rows[i].cells[4];
-            
-            rows[i].cells.zlata = rows[i].cells[7];
-            rows[i].cells.many = rows[i].cells[8];
-            rows[i].cells.pop = rows[i].cells[9];
-            
-            rows[i].data = {
-                zkusenost: parseFloat(rows[i].cells.zkusenost.textContent),
-                druh: rows[i].cells.druh.textContent,
-                typ: rows[i].cells.typ.textContent,
-                
-                zlata: parseFloat(rows[i].cells.zlata.textContent),
-                many: parseFloat(rows[i].cells.many.textContent),
-                pop: parseFloat(rows[i].cells.pop.textContent)
-            };
-            
-            context.jednotky.push(rows[i]);
-        }
-        
-        return context.jednotky.length > 0;
+        context.zaTah.zlata = parseFloat(context.zaTah.tdZlata.textContent);
+        context.zaTah.many = parseFloat(context.zaTah.tdMany.textContent);
+        context.zaTah.pop = parseFloat(context.zaTah.tdPop.textContent);
+    
+        return true;
     },
 
     process: function(page, context) {
-        var rows = page.hospodarstvi.tableJednotky.rows;
+        // Obarvy jednotky
+        var jednotky = page.hospodarstvi.jednotky;
         
-        context.jednotky.each(function(r) {
+        jednotky.each(function(r) {
                 r.cells.zkusenost.style.color = Color.fromRange(r.data.zkusenost / 100, 0.30, 0.95, Color.Pickers.redGreen);
                 r.cells.druh.className = (r.data.druh == "Let." ? "druhLet" : "druhPoz");
                 r.cells.typ.className = (r.data.typ == "Str." ? "typStr" : "typBoj");
                 
-                var zlataKoef = r.data.zlata / (context.zlataCelkem / context.pocetJednotek);
-                var manyKoef = r.data.many / (context.manyCelkem / context.pocetJednotek);
-                var popKoef = r.data.pop / (context.popCelkem / context.pocetJednotek);
+                var zlataKoef = r.data.zlata / (jednotky.zlataCelkem / jednotky.length);
+                var manyKoef = r.data.many / (jednotky.manyCelkem / jednotky.length);
+                var popKoef = r.data.pop / (jednotky.popCelkem / jednotky.length);
                 
                 r.cells.zlata.style.color = Color.fromRange(zlataKoef, 0.2, 2.0, Color.Pickers.grayGold);
                 r.cells.many.style.color = Color.fromRange(manyKoef, 2.0, 0.2, Color.Pickers.blueWhite);
                 r.cells.pop.style.color = Color.fromRange(popKoef, 2.0, 0.2, Color.Pickers.whiteBrown);
+            });
+            
+        // Zcervenej zaporne zisky
+        if (context.zaTah.zlata < 0)
+            context.zaTah.tdZlata.style.color = "red";
+        if (context.zaTah.many < 0)
+            context.zaTah.tdMany.style.color = "red";
+        if (context.zaTah.pop < 0)
+            context.zaTah.tdPop.style.color = "red";
+    }
+}));
+
+pageExtenders.add(PageExtender.create({
+    getName: function() { return "Hospodarstvi - Jednotky"; },
+
+    analyze: function(page, context) {
+        return page.hospodarstvi && page.hospodarstvi.jednotky.length > 0;
+    },
+
+    process: function(page, context) {
+        page.hospodarstvi.jednotky.each(function(r) {
+                var link = MaPlus.Tooltips.createActiveUnit(page, r.data.jmeno);
+                if (link) {
+                    var font = r.cells.jmeno.firstChild;
+                    font.replaceChild(link, font.firstChild);
+                }
             });
     }
 }));
@@ -161,115 +219,3 @@ pageExtenders.add(PageExtender.create({
         page.hospodarstvi.tablePrijem.width = "98%";
     }
 }));
-
-/*
-
-function status_process(page) {
-    var tableStavby = page.evaluateSingle('table[1]', page.content);
-    var tableJednotky = page.evaluateSingle('table[2]', page.content);
-    var tablePrijem = page.evaluateSingle('table[3]', page.content);
-    
-    if (!tableStavby || !tableJednotky || !tablePrijem)
-        return;
-        
-    // Aby se dali lip oznacovat jednotky
-    page.leftMenu.width = "17%";
-    page.rightMenu.width = "17%";
-    page.content.width = "67%";
-    
-    tableStavby.width = "98%";
-    tableJednotky.width = "98%";
-    tablePrijem.width = "98%";
-    
-    // Pomer farmy/mesta
-    var farmyPocet = parseInt(page.evaluateString('tbody/tr[3]/td[2]/font', tableStavby));
-    var mestaPocet = parseInt(page.evaluateString('tbody/tr[4]/td[2]/font', tableStavby));
-    var pomer = farmyPocet / mestaPocet;
-    
-    if (!isNaN(pomer)) {
-        var html = '<td><font size="2">Farem na město</font></td><td align="center"><font size="2">' + pomer.toFixed(1) + '</font></td><td colspan="2"/>';
-        var row = page.addElementByXPath(tableStavby, 'tbody', "tr", "", 'tbody/tr[5]', [["bgcolor", "#303030"]]);
-        row.innerHTML = html;
-    }
-    
-    // Obravit ceny jednotek
-    var jednotkyRows = page.evaluate('tbody/tr', tableJednotky);
-    var celkemRow = jednotkyRows[jednotkyRows.length - 1];
-    
-    var jednotek = jednotkyRows.length - 2;
-    var zlataCelkem = parseFloat(page.evaluateSingle('td[5]/font', celkemRow).textContent);
-    var manyCelkem = parseFloat(page.evaluateSingle('td[6]/font', celkemRow).textContent);
-    var popCelkem = parseFloat(page.evaluateSingle('td[7]/font', celkemRow).textContent);
-    
-    for (var i in jednotkyRows) {
-        if (page.evaluateNumber('count(td)', jednotkyRows[i]) < 10)
-            continue;
-            
-        // Aktivni jednotka
-        var eJednotka = page.evaluateSingle('td[1]/font', jednotkyRows[i]);
-        
-        var eTooltip = createActiveUnit(page, eJednotka.textContent);
-        if (eTooltip) {
-            eJednotka.replaceChild(eTooltip, eJednotka.firstChild);
-        }
-    
-        // Obarvy zkusenost a druh
-        var zkusenostElem = page.evaluateSingle('td[2]/font', jednotkyRows[i]);
-        var druhElem = page.evaluateSingle('td[4]/font', jednotkyRows[i]);
-        var typElem = page.evaluateSingle('td[5]/font', jednotkyRows[i]);
-        
-        zkusenostElem.style.color = colorByRange(parseFloat(zkusenostElem.textContent) / 100, 0.30, 0.95, redGreenColorPicker);
-        
-        if (druhElem.textContent == "Let.")
-            druhElem.className = "druhLet";
-        else
-            druhElem.className = "druhPoz";
-            
-        if (typElem.textContent == "Str.")
-            typElem.className = "typStr";
-        else
-            typElem.className = "typBoj";
-            
-        // Obarvy vydaje
-        var zlataElem = page.evaluateSingle('td[8]/font', jednotkyRows[i]);
-        var manyElem = page.evaluateSingle('td[9]/font', jednotkyRows[i]);
-        var popElem = page.evaluateSingle('td[10]/font', jednotkyRows[i]);
-        
-        var zlatoKoef = parseFloat(zlataElem.textContent) / (zlataCelkem / jednotek);
-        zlataElem.style.color = colorByRange(zlatoKoef, 0.2, 2.0, grayGoldColorPicker);
-        
-        var manaKoef = parseFloat(manyElem.textContent) / (manyCelkem / jednotek);
-        manyElem.style.color = colorByRange(manaKoef, 2.0, 0.2, blueWhiteColorPicker);
-        
-        var popKoef = parseFloat(popElem.textContent) / (popCelkem / jednotek);
-        popElem.style.color = colorByRange(popKoef, 2.0, 0.2,
-            function (colors) {
-                // Color picker 
-                colors.red = Math.max(167, Math.min(240, 240 * colors.koeficient));
-                colors.green = Math.max(118, Math.min(240, 240 * colors.koeficient));
-                colors.blue = Math.max(109, Math.min(240, 240 * colors.koeficient)); 
-            });
-            
-        // Cena za 1 sily
-        
-       // var sila = parseFloat(page.evaluateSingle('td[6]/font', jednotkyRows[i]).textContent);
-       // var cenaZaTisicSily = -parseFloat(zlataElem.textContent) / sila * 1000;
-        
-     //   var cenaZaSileElem = page.addElement(jednotkyRows[i], "td", '<font size="2">' + cenaZaTisicSily.toFixed(1) + '</font>');
-     //   cenaZaSileElem.style.color = colorByRange(cenaZaTisicSily, 125, 30, redGreenColorPicker);
-        
-    }
-    
-    // Zcervenej zaporne zisky
-    var zlataZaTahElem = page.evaluateSingle('tbody/tr[5]/td[2]/font', tablePrijem);
-    var manyZaTahElem = page.evaluateSingle('tbody/tr[5]/td[3]/font', tablePrijem);
-    var popZaTahElem = page.evaluateSingle('tbody/tr[5]/td[4]/font', tablePrijem);
-    
-    if (parseFloat(zlataZaTahElem.textContent) < 0)
-        zlataZaTahElem.style.color = 'red';
-    if (parseFloat(manyZaTahElem.textContent) < 0)
-        manyZaTahElem.style.color = 'red';
-    if (parseFloat(popZaTahElem.textContent) < 0)
-        popZaTahElem.style.color = 'red';
-}
-*/
