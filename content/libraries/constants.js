da77e1be9c3f0d13bdcae157c8fed5200d86a47e
ext91596 <ext91596@BUDWD284>
2007-11-30 14:47:06 +0100

    Verze 1.2.0 Upraveny novinky

--- content/libraries/constants.js
+++ content/libraries/constants.js
@@ -34,7 +34,7 @@
  * 
  * ***** END LICENSE BLOCK ***** */
 
-const VERSION = "1.2.0pre2";
+const VERSION = "1.2.0";
 
 const EXTENSION_NAME = "maplus";
 const EXTENSION_ID = "maplus@michal.dvorak";
