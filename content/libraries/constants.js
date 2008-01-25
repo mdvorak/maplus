f28a5d3d2b9d1b6be872a3c7a9aada08e751743e
Mikee <Mikee@MIKEE-PC>
2008-01-25 21:44:33 +0100

    Jeste jednou zmeneno moje ID

--- content/libraries/constants.js
+++ content/libraries/constants.js
@@ -43,8 +43,8 @@ const MELIOR_ANNIS_URL = "http://meliorannis.idnes.cz";
 const CHROME_URL = "chrome://" + EXTENSION_NAME + "/";
 const CHROME_CONTENT_URL = CHROME_URL + "content/";
 
-const ADMIN_ID = 2131; // Moje id
-const ADMIN_ID_AGE = "5.6"; // Jmeno veku pro ktere je ID platne
+const ADMIN_ID = 1212; // Moje id
+const ADMIN_ID_AGE = "5.7"; // Jmeno veku pro ktere je ID platne
 
 // Custom constants
 const ZADNA_ALIANCE = "##ZADNA_ALIANCE##";
--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -2,4 +2,3 @@
 pref("extensions.maplus.debug_marshal", 0);
 pref("extensions.maplus.debug_xpath", 0);
 pref("extensions.maplus.age_name", "5.7");
-pref("extensions.maplus.admin_id", "1212");
