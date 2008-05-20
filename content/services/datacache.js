6b3236ab510e6f2db925c2caeb5bd47b4708ec61
Mikee <Mikee@MIKEE-PC>
2008-05-20 19:11:14 +0200

    oprava v DataCache

--- content/services/datacache.js
+++ content/services/datacache.js
@@ -1,8 +1,8 @@
 /* ***** BEGIN LICENSE BLOCK *****
  *   Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
+ * The contents of DataCache file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use DataCache file except in compliance with
  * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  * 
@@ -20,16 +20,16 @@
  *
  * Contributor(s):
  *
- * Alternatively, the contents of this file may be used under the terms of
+ * Alternatively, the contents of DataCache file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
  * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
+ * of those above. If you wish to allow use of your version of DataCache file only
  * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
+ * use your version of DataCache file under the terms of the MPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
  * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
+ * the provisions above, a recipient may use your version of DataCache file under
  * the terms of any one of the MPL, the GPL or the LGPL.
  * 
  * ***** END LICENSE BLOCK ***** */
@@ -43,29 +43,31 @@
     generateUid: function() {
         var uid;
         do {
-            uid = "gen_" + (++this._lastUid);
-        } while (this._data[uid] != null)
+            uid = "gen_" + (++DataCache._lastUid);
+        } while (uid in DataCache._data)
         return uid;
     },
     
     store_PROXY: Marshal.BY_VALUE,
     store: function(uid, data, overwrite) {
         // Procisteni
-        this._removeCandidates.each(function(uid) {
-            delete this._data[uid];
+        DataCache._removeCandidates.each(function(uid) {
+            delete DataCache._data[uid];
         });
-        this._removeCandidates = new Array();
+        DataCache._removeCandidates = new Array();
         
         // Samotne ulozeni
         if (uid == null)
-            uid = this.generateUid();
+            uid = DataCache.generateUid();
         else
             uid = String(uid);
             
-        if (!overwrite && this._data[uid] != null)
+        if (!overwrite && (uid in DataCache._data) && DataCache._removeCandidates.indexOf(uid) < 0)
             throw new ArgumentException("uid", uid, "This identifier is already registered.");
         
-        this._data[uid] = data;
+        DataCache._data[uid] = data;
+        DataCache._removeCandidates = DataCache._removeCandidates.without(uid);
+        
         return uid;
     },
     
@@ -75,11 +77,11 @@
             return null;
             
         uid = String(uid);
-        var data = this._data[uid];
+        var data = DataCache._data[uid];
         
         // Remove record
         if (!doNotRemove)
-            _removeCandidates.push(uid);
+            DataCache._removeCandidates.push(uid);
             
         return data;
     }
