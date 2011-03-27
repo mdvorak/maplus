577b43651ea044363830b377e5396757e805f7df
Mikee <mikee2185@gmail.com>
2011-03-27 22:27:53 +0200

    1.4pre1

--- content/libraries/constants.js
+++ content/libraries/constants.js
@@ -34,7 +34,7 @@
  * 
  * ***** END LICENSE BLOCK ***** */
 
-const VERSION = "1.3.17"; // Pozn: Tahle verze je pouzivana pouze pro zobrazovani novinek
+const VERSION = "1.4pre1"; // Pozn: Tahle verze je pouzivana pouze pro zobrazovani novinek
 
 const EXTENSION_NAME = "maplus";
 const EXTENSION_ID = "maplus@michal.dvorak";
